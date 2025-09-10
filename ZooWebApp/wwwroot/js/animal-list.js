ef298c9745a83cab00aaa9d43a4bdc3aefa3fe20
Yu Zhang <zhayy374@mymail.unisa.edu.au>
2025-09-10 19:18:05 +0930

    Fetch and display animal data on animal-list page

--- ZooWebApp/wwwroot/js/animal-list.js
+++ ZooWebApp/wwwroot/js/animal-list.js
@@ -1,25 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-    //Mock data
-    const animals = [
-        {
-            name: "test1",
-            species: "Wolf",
-            image: "https://images.unsplash.com/photo-1504208434309-cb69f4fe52b0",
-            age: "4",
-        },
-        {
-            name: "test2",
-            species: "Lion",
-            image: "https://images.unsplash.com/photo-1546182990-dffeafbe841d",
-            age: "5",
-        },
-        {
-            name: "test3",
-            species: "Bear",
-            image: "https://images.unsplash.com/photo-1713062711590-b29c683708e0?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8TWFyc2ljYW4lMjBicm93biUyMGJlYXJ8ZW58MHx8MHx8fDA%3D",
-            age: "2",
-        },
-    ];
+    //Mock data from animal-data.js
+    const animals = window.mockAnimals;
 
     //Animal card
     const list = document.getElementById("animal-list");
@@ -30,13 +11,13 @@ document.addEventListener("DOMContentLoaded", function () {
             "bg-zoo-lightpink text-zoo-darkbrown rounded-lg overflow-hidden shadow-lg hover:ring-4 hover:ring-orange-600 hover:bg-orange-600 transition cursor-pointer";
 
         card.innerHTML = `
-          <img src="${animal.image}" alt="${animal.name}" class="w-full aspect-square object-cover">
+          <img src="${animal.animalImage}" alt="${animal.animalImage}" class="w-full aspect-square object-cover">
           <div class="p-4">
             <div class="flex justify-between items-center mb-2">
               <h3 class="text-xl font-bold">${animal.species}</h3>
-              <h3 class="text-xl font-medium">${animal.name}</h3>
+              <h3 class="text-xl font-medium">${animal.animalName}</h3>
             </div>
-            <span class="inline-block bg-zoo-primary text-white text-xs px-2 py-1 rounded-full">${animal.age} years old</span>
+            <span class="inline-block bg-zoo-primary text-white text-xs px-2 py-1 rounded-full">${animal.animalAge} ${animal.animalAge === 1 ? "year" : "years"} old</span>
           </div>
         `;
 
