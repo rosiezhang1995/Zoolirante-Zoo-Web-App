a831f88aaa73803130e9a2ab232e07be2d5284a5
walwy004 <walwy004@mymail.unisa.edu.au>
2025-09-12 17:39:35 +0930

    Create EventAnimal Table Schema
    Bridge table between Animal and Event to support many to many relationship

---
+++ ZooWebApp/Models/EventAnimal.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ZooWebApp.Models
+{
+    public class EventAnimal
+    {
+        [ForeignKey("Event")]
+        public int EventID { get; set; }
+        public Event Event { get; set; }
+
+        [ForeignKey("Animal")]
+        public int AnimalID { get; set; }
+        public Animal Animal { get; set; }
+    }
+}
