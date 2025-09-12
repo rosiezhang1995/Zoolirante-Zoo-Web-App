88a0ae158d2f0522146b0cb75350ae541b335584
walwy004 <walwy004@mymail.unisa.edu.au>
2025-09-12 17:31:20 +0930

    Create Event table schema

---
+++ ZooWebApp/Models/Event.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ZooWebApp.Models
+{
+    public class Event
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int EventID { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        [Required]
+        [Column(TypeName = "Date")]
+        public DateTime EventData { get; set; }
+
+        [Required]
+        [Column(TypeName = "Time")]
+        public TimeOnly EventTime { get; set; }
+
+        [Required]
+        [StringLength (100)]
+        public string Location { get; set; }
+    }
+}
