a3b6d0d6cf26d543959e5ad27c9bd29700ed1bad
Eric Hanan <haney015@mymail.unisa.edu.au>
2025-09-12 18:25:06 +0930

    Add event seeding function

---
+++ ZooWebApp/Data/EventSeed.cs
@@ -0,0 +1,21 @@
+﻿using ZooWebApp.Models;
+
+namespace ZooWebApp.Data
+{
+    public class EventSeed
+    {
+        public static void Seed(ZooWebAppContext context)
+        {
+            // Check if events already exist
+            if (context.Event.Any())
+            {
+                return;
+            }
+
+            var events = new List<Event>();
+
+            context.Event.AddRange();
+            context.SaveChanges();
+        }
+    }
+}
