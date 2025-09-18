55cfee47344ef39c3f33e6a7c5f0e5270f0c864c
walwy004 <walwy004@mymail.unisa.edu.au>
2025-09-18 11:48:43 +0930

    Add data annotations to merchandise table schema

--- ZooWebApp/Models/Merchandise.cs
+++ ZooWebApp/Models/Merchandise.cs
@@ -1,19 +1,38 @@
-﻿namespace ZooWebApp.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ZooWebApp.Models
 {
 	public class Merchandise
 	{
-        public int MerchandiseID { get; set; }
+		[Key]
+		[Display(Name = "Merchandise Id")]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int MerchandiseID { get; set; }
 
-        public string MerchandiseName { get; set; }
+		[Required]
+		[Display(Name = "Animal Name")]
+		[StringLength(50, ErrorMessage = "Merch Name must be no more than 50 characters")]
+		public string MerchandiseName { get; set; }
 
-        public string Description { get; set; }
+		[Display(Name = "Animal Description")]
+		[StringLength(500, ErrorMessage = "Description must be no more than 500 characters")]
+		public string? Description { get; set; }
 
-        public decimal Price { get; set; }
+		[Required]
+		[Display(Name = "Price")]
+		[DataType(DataType.Currency)]
+		[Column(TypeName = "decimal(10,2)")]
+		public decimal Price { get; set; }
 
-        public Boolean Avaliable { get; set; }
+		[Required]
+		[Display(Name = "Stock Availability")]
+		public Boolean Avaliable { get; set; }
 
-        public string MerchandiseImage { get; set; }
+		[Display(Name = "Animal Image")]
+		public string? MerchandiseImage { get; set; }
 
-        public string? Size { get; set; }
+		[Display(Name = "Size")]
+		public string? Size { get; set; }
     }
 }
