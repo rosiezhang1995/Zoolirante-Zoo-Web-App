deec8e404fbe13882fadb301c9880b5ef4e73f69
Eric Hanan <haney015@mymail.unisa.edu.au>
2025-09-26 14:31:30 +0930

    Add new migration to support User table

--- ZooWebApp/Data/ZooWebAppContext.cs
+++ ZooWebApp/Data/ZooWebAppContext.cs
@@ -17,5 +17,6 @@ namespace ZooWebApp.Data
         public DbSet<ZooWebApp.Models.Animal> Animal { get; set; } = default!;
         public DbSet<ZooWebApp.Models.Event> Event { get; set; } = default!;
         public DbSet<ZooWebApp.Models.Merchandise> Merchandise { get; set; } = default!;
+        public DbSet<ZooWebApp.Models.User> User { get; set; } = default!;
     }
 }
