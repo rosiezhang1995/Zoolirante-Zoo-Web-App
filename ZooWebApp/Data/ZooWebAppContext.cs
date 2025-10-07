5ad68cb9a358fb3cd0a39f11c082a9f48f5d60f7
Yurosiezhang <147775545+Yurosiezhang@users.noreply.github.com>
2025-10-07 15:51:22 +1030

    Revert "Pull Backend/ticket booking to dev"

--- ZooWebApp/Data/ZooWebAppContext.cs
+++ ZooWebApp/Data/ZooWebAppContext.cs
@@ -18,53 +18,5 @@ namespace ZooWebApp.Data
         public DbSet<ZooWebApp.Models.Event> Event { get; set; } = default!;
         public DbSet<ZooWebApp.Models.Merchandise> Merchandise { get; set; } = default!;
         public DbSet<ZooWebApp.Models.User> User { get; set; } = default!;
-
-
-        // booking system
-        public DbSet<ZooWebApp.Models.TicketType> TicketTypes { get; set; } = default!;
-        public DbSet<ZooWebApp.Models.PaymentMethod> PaymentMethods { get; set; } = default!;
-        public DbSet<ZooWebApp.Models.Booking> Bookings { get; set; } = default!;
-        public DbSet<ZooWebApp.Models.BookingItem> BookingItems { get; set; } = default!;
-
-        // Relationships
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            modelBuilder.Entity<BookingItem>()
-                .HasOne(bi => bi.Booking)
-                .WithMany(b => b.Items)
-                .HasForeignKey(bi => bi.BookingID)
-                .OnDelete(DeleteBehavior.Cascade);
-
-            modelBuilder.Entity<BookingItem>()
-                .HasOne(bi => bi.TicketType)
-                .WithMany()
-                .HasForeignKey(bi => bi.TicketTypeID)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            modelBuilder.Entity<Booking>()
-                .HasIndex(b => b.BookingReference)
-                .IsUnique();
-
-            modelBuilder.Entity<PaymentMethod>()
-                .HasOne(pm => pm.User)
-                .WithMany()
-                .HasForeignKey(pm => pm.UserID)
-                .OnDelete(DeleteBehavior.Cascade);
-
-            modelBuilder.Entity<Booking>()
-                .HasOne(b => b.User)
-                .WithMany()
-                .HasForeignKey(b => b.UserID)
-                .OnDelete(DeleteBehavior.Restrict);  
-
-            modelBuilder.Entity<Booking>()
-                .HasOne(b => b.PaymentMethod)
-                .WithMany()
-                .HasForeignKey(b => b.PaymentMethodID)
-                .OnDelete(DeleteBehavior.Restrict); 
-        }
-
     }
 }
