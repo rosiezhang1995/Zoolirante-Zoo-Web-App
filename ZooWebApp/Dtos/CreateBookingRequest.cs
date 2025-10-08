8d9593dd9433b64ae4c5ef4a0be3f24ac4956797
Yu Zhang <zhayy374@mymail.unisa.edu.au>
2025-10-08 15:56:20 +1030

    Delete unnecessary IsDefault option

--- ZooWebApp/Dtos/CreateBookingRequest.cs
+++ ZooWebApp/Dtos/CreateBookingRequest.cs
@@ -44,17 +44,20 @@ namespace ZooWebApp.Models.DTOs
         public int UserID { get; set; }
 
         [Required]
+        [StringLength(200)]
         public string CardHolderName { get; set; }
 
         [Required]
+        [StringLength(19, MinimumLength = 13)]
         public string CardNumber { get; set; }
 
         [Required]
+        [Range(1, 12)]
         public int ExpiryMonth { get; set; }
 
         [Required]
+        [Range(2024, 2050)]
         public int ExpiryYear { get; set; }
 
-        public bool IsDefault { get; set; }
     }
 }
